d3c8015467ac64be2bf031629832d1940a234d75
Kevin T. Coughlin <706967+KevinTCoughlin@users.noreply.github.com>
2025-06-29 18:28:10 +0000

    WIP

--- packages/fluentui-compat/src/index.ts
+++ packages/fluentui-compat/src/index.ts
@@ -1,9 +1,11 @@
-export { bundleIcon } from './bundleIcon';
-export type { BundledIconProps } from './bundleIcon';
-export { useAsync } from './useAsync';
-export { useBoolean } from './useBoolean';
-export type { IUseBooleanCallbacks } from './useBoolean';
-export { useConst } from './useConst';
-export { useForceUpdate } from './useForceUpdate';
-export { useSetTimeout } from './useSetTimeout';
-export type { UseSetTimeoutReturnType, TimeoutId } from './useSetTimeout';
+export { bundleIcon } from "./bundleIcon";
+export type { BundledIconProps } from "./bundleIcon";
+export { useAsync } from "./useAsync";
+export { useBoolean } from "./useBoolean";
+export type { IUseBooleanCallbacks } from "./useBoolean";
+export { useConst } from "./useConst";
+export { useForceUpdate } from "./useForceUpdate";
+export { useSetTimeout } from "./useSetTimeout";
+export type { UseSetTimeoutReturnType, TimeoutId } from "./useSetTimeout";
+
+export { useEventCallback } from "./useEventCallback";
